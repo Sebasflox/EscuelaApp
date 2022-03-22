76c1dcc4cba63e4a381fd251a23475eaac3bc374
Sebasflox <86434941+Sebasflox@users.noreply.github.com>
2022-03-22 17:15:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,43 @@
-<?php include "shared/header.php" ?>
+<?php include "Shared/header.php" ?>
+
+<?php 
+    
+    if (!empty($_POST)){
+        
+        $usr = $_POST["user"];
+        $pass = $_POST["pass"];
+
+        if (strcmp($usr, "admin") == 0 && strcmp($pass, "ADmin") == 0){
+            header("Location: PaginaEstudiantes.php");
+        }
+        else{
+            $error = "1";
+        }
+    }
+
+?>
 
 <main>
-    <h1>Bienvenidos a la EscuelaApp</h1>
+    <h1>Iniciar Sesión</h1>
 
-    
+    <form action="" method= "POST"> 
+        <div>
+            <Label for="">Usuario</Label>
+            <input type="text" name="user">
+        </div>
+        <div>
+            <Label for="">Clave</Label>
+            <input type="password" name="pass">
+        </div>
+        <div>
+            <input type="submit" value="Iniciar Sesión">
+        </div>
+    </form>
+
+    <?php if (isset($error)) { ?>
+
+        <p>Usuario o contraseña incorrecta</p>
+
+    <?php } ?>
+
 </main>
-
-<?php include "shared/footer.php" ?>
-
-    
-
-    

e3a412208da6124530de054faf898bc0cb2cb1f0
Sebasflox <86434941+Sebasflox@users.noreply.github.com>
2022-03-22 18:13:49 -0600

    Update footer.php

--- shared/footer.php
+++ shared/footer.php
@@ -1,13 +1,11 @@
-
-
 <footer class="footer-basic-centered">
 
-        <p class="footer-company-motto">Derechos Reservados</p>
+<p class="footer-company-motto">Derechos Reservados</p>
 
 
-        <p class="footer-company-name">&copy; 2022</p>
+<p class="footer-company-name">&copy; 2022</p>
 
-    </footer>
+</footer>
 
 </body>
 

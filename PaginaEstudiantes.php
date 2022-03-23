c2314c57869585500fbaf5b53ad619bac65f1441
Sebasflox <86434941+Sebasflox@users.noreply.github.com>
2022-03-22 18:13:53 -0600

    Update PaginaEstudiantes.php

--- PaginaEstudiantes.php
+++ PaginaEstudiantes.php
@@ -1,46 +1,35 @@
 <?php include "Shared/header.php" ?>
 
+<?php 
+
+    if (!empty($_Seleccion)){
+
+        $dato = $_Seleccion["1"];
+
+        if (strcmp($dato, "1") == 0){
+            header("Location: resultados.php?cat=Primero");
+        }
+    }
+
+?>
+
 <main>
     <h1>Pagina Estudiantes</h1>
 
-    <form action="" method= "POST"> 
-        <div>
-            <Label for="">Nombre</Label>
-            <input type="text" name="usr">
-        </div>
-        <div>
-            <Label for="">Primer Apellido</Label>
-            <input type="text" name="usr">
-        </div>
-        <div>
-            <Label for="">Segundo Apellido</Label>
-            <input type="text" name="usr">
-        </div>
-        <div>
-            <label for="">Correo Electronico</label>
-            <input type="text" namme="usr">
-        </div>
-        <div>
-            <label for="">Contraseña</label>
-            <input type="text" name="usr">
-        </div>
-        <div>
-            <label for="">celular</label>
-            <input type="text" name="usr">
-        </div>
+    <form action="" method= "POST">
 
+    <select method="Seleccion" class="form-select" aria-label="Default select example">
+        <option selected>Seleccione un grado</option>
+        <option href="resultados.php?cat=Primero">Primero</option>
+        <option name="2">Segundo</option>
+        <option name="3">Tercero</option>
+        
 
-        <div>
-            <input type="submit" value="Registrarse">
-        </div>
-        <div>
-            <button onclick>Volver</button>
-            <input type="submit" value="Volver">
-        </div>
+        <input type="submit" value="Seleccionar">
+        <button onclick="index.php">Volver</button>
+
+    </select>
 
     </form>
-
-
-
 
 </main>
